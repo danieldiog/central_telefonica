8b4f9a0dbc083ec5a750cc30286a3110ff9559f5
Daniel Diógenes <danieldiogenes.dd@gmail.com>
2024-11-21 23:19:10 -0300

    criação do compose e arquivos configurados de banco e asterisk

--- base-postgresql/entrypointdb.sh
+++ base-postgresql/entrypointdb.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-# Verificar se o banco de dados já foi inicializado
+
+# Inicializa o banco de dados, se necessário
 if [ ! -f "$PGDATA/PG_VERSION" ]; then
     echo "Inicializando o banco de dados..."
-    /usr/pgsql-${POSTGRES_VERSION}/bin/initdb -D $PGDATA
+    /usr/pgsql-17/bin/initdb -D $PGDATA
+
+    echo "Copiando arquivos de configuração personalizados..."
+    cp /tmp/pg_hba.conf $PGDATA/pg_hba.conf
+    cp /tmp/postgresql.conf $PGDATA/postgresql.conf
+    chown postgres:postgres $PGDATA/pg_hba.conf $PGDATA/postgresql.conf
 fi
 
-# Executar o comando padrão do PostgreSQL
-exec "$@"
+# Iniciar o PostgreSQL
+echo "Iniciando o PostgreSQL..."
+exec /usr/pgsql-17/bin/postgres -D $PGDATA
