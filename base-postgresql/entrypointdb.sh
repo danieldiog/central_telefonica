2169ef1db329afc99fc57cd32bcefeb2ef0ebb46
danieldiog <96247838+danieldiog@users.noreply.github.com>
2024-11-25 17:09:23 -0300

    Update entrypointdb.sh
    Ajustes script entrypoint

--- base-postgresql/entrypointdb.sh
+++ base-postgresql/entrypointdb.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Inicializa o banco de dados, se necessário
 if [ ! -f "$PGDATA/PG_VERSION" ]; then
     echo "Inicializando o banco de dados..."
     /usr/pgsql-17/bin/initdb -D $PGDATA
@@ -10,7 +9,4 @@ if [ ! -f "$PGDATA/PG_VERSION" ]; then
     cp /tmp/postgresql.conf $PGDATA/postgresql.conf
     chown postgres:postgres $PGDATA/pg_hba.conf $PGDATA/postgresql.conf
 fi
-
-# Iniciar o PostgreSQL
-echo "Iniciando o PostgreSQL..."
 exec /usr/pgsql-17/bin/postgres -D $PGDATA
